36c97ddc1f36e6940451525e75cac1af5bb7f9a8
Pelle Hald Vedsmand <cph-pv73@cphbusiness.dk>
2024-02-28 12:40:54 +0100

    Added visualization

--- Code/dataCleaning.py
+++ Code/dataCleaning.py
@@ -34,18 +34,18 @@ print(megaGym_datasetDF.describe)
 print(exercise_datasetDF.describe)
 
 # Removing non numerical columns for the exercise dataset
-nonNumericExerciseDF = exercise_datasetDF._get_numeric_data()
+numericExerciseDF = exercise_datasetDF._get_numeric_data()
 print(exercise_datasetDF)
 
 
-nonNumericExerciseDF.plot(kind='box', figsize=(20, 10))
+numericExerciseDF.plot(kind='box', figsize=(20, 10))
 plt.tight_layout()
 plt.show()
 
 # Checking for outliers
 sc = StandardScaler()
-StandardDF = sc.fit_transform(nonNumericExerciseDF)
-StandardDF = pd.DataFrame(StandardDF, columns=nonNumericExerciseDF.columns)
+StandardDF = sc.fit_transform(numericExerciseDF)
+StandardDF = pd.DataFrame(StandardDF, columns=numericExerciseDF.columns)
 StandardDF.plot(kind='box', figsize=(20, 10))
 plt.tight_layout()
 plt.show()
@@ -64,13 +64,24 @@ print(outliers)
 # We wont remove the outliers, since they are a real exercise, and they are adding value to the dataset, by being a productive exercise for our users
 
 # Lets check for correlation
-nonNumericExerciseDF.hist(figsize=(15, 10))
+numericExerciseDF.hist(figsize=(15, 10))
 plt.tight_layout()
 plt.show()
 plt.figure(figsize=(15, 10))
-sns.heatmap(nonNumericExerciseDF.corr(), annot=True)
+sns.heatmap(numericExerciseDF.corr(), annot=True)
 plt.show()
 
 # On the correlation map, we can see that the the exercise dataset holds a lot of correlation between the different exercises, which is expected, since the exercises are similar in nature, and should be correlated
 # The correlation between the body weights and amount of calories burned are 1=1
+
+# We need to find the exercises the users can actually do at the gym
+exercise_types = ['Stationary cycling', 'Calisthenics', 'Circuit training', 'Weight lifting', 'Stair machine', 'Rowing machine', 'Ski machine', 'Aerobics', 'Stretching', 'Mild stretching', 'Instructing aerobic class', 'Running', 'Martial arts']
+
+gymExercises = exercise_datasetDF[exercise_datasetDF['Activity, Exercise or Sport (1 hour)'].str.contains('|'.join(exercise_types))]
+
+# Now we have the exercises that the users can do at the gym called gymExercises
+# Lets visualize the exercises that the users can do at the gym and the amount of calories they burn
+plt.figure(figsize=(15, 10))
+sns.barplot(x='Calories per kg', y='Activity, Exercise or Sport (1 hour)', data=gymExercises)
+plt.show()
 
